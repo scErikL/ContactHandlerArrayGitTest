9e736bf11efb9deef8075835812e0e4d6bd1f051
larare <larare@S150924-HP009>
2017-02-17 14:13:51 +0100

    Andra commit

--- Github_test/src/github/HelloWorld.java
+++ Github_test/src/github/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello Github");
+			
+			System.out.println("Hello Github");
+			
 	}
 
 }
